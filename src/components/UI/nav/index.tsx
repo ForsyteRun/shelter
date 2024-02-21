66242b54278ea39a7e6063e258c35743a893b72e
Dripa Alexandr <forsyteRun@gmail.com>
2024-02-21 15:01:07 +0200

    style: adaptive header & present block 1280-768px

--- src/components/UI/nav/index.tsx
+++ src/components/UI/nav/index.tsx
@@ -4,13 +4,15 @@ import s from "./styles.module.scss";
 
 const Nav = () => {
   return (
-    <div className={s.container}>
-      {headerNavLinks.map(({ title, path }) => (
-        <Link to={path} key={title}>
-          {title}
-        </Link>
-      ))}
-    </div>
+    <>
+      <div className={s.container}>
+        {headerNavLinks.map(({ title, path }) => (
+          <Link to={path} key={title}>
+            {title}
+          </Link>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/components/layout/present/index.tsx
+++ src/components/layout/present/index.tsx
@@ -6,7 +6,9 @@ const Present = () => {
   return (
     <section className={s.container}>
       <div className={s.content}>
-        <h1>Not only people need a house</h1>
+        <h1>
+          Not only people <span>need a house</span>
+        </h1>
         <p>
           We offer to give a chance to a little and nice puppy with an extremely
           wide and open heart. He or she will love you more than anybody else in
