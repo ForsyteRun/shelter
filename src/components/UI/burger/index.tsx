062404eac2152044d831479491b921c7bb390363
Dripa Alexandr <forsyteRun@gmail.com>
2024-02-21 16:31:26 +0200

    style: burgernenu appear

--- src/components/UI/burger/index.tsx
+++ src/components/UI/burger/index.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import s from "./styles.module.scss";
+import cn from "classnames";
 
 const Burger = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div className={s.container}>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
+    <>
+      <div className={s.container} onClick={handleClick}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={cn({ [s.menu]: open })}></div>
+    </>
   );
 };
 
