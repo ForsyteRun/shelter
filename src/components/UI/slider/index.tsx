b03f52ee711e271679d291c103739cd52ed963bc
Dripa Alexandr <forsyteRun@gmail.com>
2024-02-23 11:14:47 +0200

    style: adaptive slider 768-1280

--- src/components/UI/slider/index.tsx
+++ src/components/UI/slider/index.tsx
@@ -1,6 +1,5 @@
 import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
-import s from "./styles.module.scss";
 import cn from "classnames";
 import { sliderData } from "@/db/slider";
 import { Card } from "@/components/layout";
@@ -8,15 +7,21 @@ import { Card } from "@/components/layout";
 const Slider = () => {
   const [ref] = useKeenSlider<HTMLDivElement>({
     breakpoints: {
-      "(min-width: 1000px)": {
+      "(min-width: 1180px)": {
         slides: { perView: 3, spacing: 90 },
       },
+      "( max-width: 1180px)": {
+        slides: { perView: 2, spacing: 30 },
+      },
+      // "(max-width: 890px)": {
+      //   slides: { perView: 3, spacing: 20 },
+      // },
       // "(max-width: 1000px)": {
       //   slides: { perView: 2, spacing: 5 },
       // },
-      "(max-width: 730px)": {
-        slides: { perView: 1, spacing: 5 },
-      },
+      // "(max-width: 730px)": {
+      //   slides: { perView: 1, spacing: 5 },
+      // },
     },
     loop: true,
   });
