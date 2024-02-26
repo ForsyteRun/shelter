f4e17deb48da25f0f3afe0de0e2910d3f2787a67
Dripa Alexandr <forsyteRun@gmail.com>
2024-02-26 19:52:19 +0200

    adaptive: footer 768-1280px

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -7,8 +7,16 @@ import { footerInfo } from "@/db/footer";
 const Footer = () => {
   return (
     <div className={cn("container", s.container)}>
-      <FooterInfo data={footerInfo[0]} styles={cn(s.content, s.contentLeft)} />
-      <FooterInfo data={footerInfo[1]} styles={cn(s.content, s.contentRight)} />
+      <div className={s.contentContainer}>
+        <FooterInfo
+          data={footerInfo[0]}
+          styles={cn(s.content, s.contentLeft)}
+        />
+        <FooterInfo
+          data={footerInfo[1]}
+          styles={cn(s.content, s.contentRight)}
+        />
+      </div>
       <div className={s.imageWrapper}>
         <img src={puppy} alt="puppy-dog" />
       </div>
