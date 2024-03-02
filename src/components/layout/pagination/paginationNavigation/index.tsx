fe23965c9c1fc29daa29a957aacb7da0d6ed9d35
Dripa Alexandr <forsyteRun@gmail.com>
2024-03-02 18:08:38 +0200

    feat: add  pagination natigation assets

--- src/components/layout/pagination/paginationNavigation/index.tsx
+++ src/components/layout/pagination/paginationNavigation/index.tsx
@@ -1,5 +1,22 @@
+import {
+  buttonLeft,
+  buttonLeftStart,
+  buttonRight,
+  buttonRightEnd,
+  buttonNumber,
+} from "@/assets/pagination";
+import s from "./styles.module.scss";
+
 const PaginationNavigation = () => {
-  return <div>paginationNavigation</div>;
+  return (
+    <div className={s.container}>
+      <img src={buttonLeftStart} alt="buttonLeftStart" />
+      <img src={buttonLeft} alt="buttonLeft" />
+      <img src={buttonNumber} alt="buttonNumber" />
+      <img src={buttonRight} alt="buttonRight" />
+      <img src={buttonRightEnd} alt="buttonRightEnd" />
+    </div>
+  );
 };
 
 export default PaginationNavigation;
