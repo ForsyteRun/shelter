15b9058fdae96da4a79458a224c657711646c778
Dripa Alexandr <forsyteRun@gmail.com>
2024-03-04 16:26:49 +0200

    fix: correct href to mail & tel footer

--- src/components/layout/footer/footerInfo/index.tsx
+++ src/components/layout/footer/footerInfo/index.tsx
@@ -1,10 +1,35 @@
 import { IFooterInfo, IFooterInfoContent } from "@/types/interfaces";
 import { FC } from "react";
+import s from "./styles.module.scss";
 
 interface IProps {
   data: IFooterInfo;
   styles: string;
 }
+
+const addCorrectTag = (content: string) => {
+  const fullString = content.replaceAll(" ", "");
+  const isPhoneNumber =
+    String(parseInt(fullString)).split("").length === fullString.length - 1;
+
+  const isEmail = content.includes("@");
+
+  if (isEmail) {
+    return (
+      <a href={`mailto:${content}`} className={s.footerLinks}>
+        {content}
+      </a>
+    );
+  } else if (isPhoneNumber) {
+    return (
+      <a href={`tel:${content}`} className={s.footerLinks}>
+        {content}
+      </a>
+    );
+  } else {
+    return <h3 className={s.footerAdress}>{content}</h3>;
+  }
+};
 
 const FooterInfo: FC<IProps> = ({ data, styles }) => {
   const { info, title } = data;
@@ -15,7 +40,7 @@ const FooterInfo: FC<IProps> = ({ data, styles }) => {
       {info.map(({ logo, content }: IFooterInfoContent, index: number) => (
         <div key={index}>
           <img src={logo} alt="logo" />
-          <h3>{content}</h3>
+          {addCorrectTag(content)}
         </div>
       ))}
     </div>
